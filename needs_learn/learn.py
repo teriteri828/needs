6580ff49486813d2c366345b785912770af3b06c
tomoya <q27d6bf86fj75@gmail.com>
2021-02-19 22:53:13 +0900

    mod ai paramerter

--- needs_learn/learn.py
+++ needs_learn/learn.py
@@ -14,7 +14,7 @@ class Learn:
 
         # モデルの作成
         model = Sequential()
-        model.add(Dense(512, activation="relu", input_shape=(512,)))
+        model.add(Dense(1024, activation="relu", input_shape=(512,)))
         model.add(Dense(512, activation="relu"))
         model.add(Dense(NUM_CLASSES, activation="softmax"))
 
@@ -24,6 +24,11 @@ class Learn:
         return model
 
     def execute(self, X_train, X_test, y_train, y_test):
+        print("X_train: {}".format(len(X_train))) 
+        print("y_train: {}".format(len(y_train))) 
+        print("X_test: {}".format(len(X_test))) 
+        print("y_test: {}".format(len(y_test))) 
+
         model = self._model_define()
         # 学習
         early_stopping = EarlyStopping(monitor="val_loss", min_delta=0.0, patience=5,)
@@ -31,11 +36,12 @@ class Learn:
         history = model.fit(
             X_train,
             y_train,
-            batch_size=1000,
+            batch_size=1500,
             epochs=100,
             verbose=2,
             validation_data=(X_test, y_test),
             callbacks=[early_stopping],
         )
+        
         model.save(SAVE_MODEL_PATH)
         return True
--- needs_learn/learn_data_split.py
+++ needs_learn/learn_data_split.py
@@ -2,11 +2,13 @@ from dataclasses import dataclass
 import numpy as np
 import glob
 from const import  USE_URL, NUM_CLASSES
+import re
 
 import tensorflow_hub as hub
 import tensorflow_text
 from sklearn.model_selection import train_test_split
 import keras
+import MeCab
 
 embed = hub.load(USE_URL)
 
@@ -16,6 +18,14 @@ class LearnDataSplit:
         num_classes = NUM_CLASSES
 
         tweets = df_learn["text"].values
+        
+        # データクリーン処理。精度落ちたので、いったんコメントアウト
+        # sentences = []
+        # for tweet in tweets:
+        #     sentences.append(self._word_extract_join(tweet))
+        # X = embed(sentences).numpy()
+
+
         X = embed(tweets).numpy()
         y = df_learn["needs"].values
         X_train, X_test, y_train, y_test = train_test_split(
@@ -28,3 +38,44 @@ class LearnDataSplit:
 
         return X_train, X_test, y_train, y_test
 
+    def _word_extract_join(self, text):
+        # 最新辞書の追加に関する参考情報
+        # https://qiita.com/SUZUKI_Masaya/items/685000d569452585210c
+        # https://www.saintsouth.net/blog/morphological-analysis-by-mecab-and-mecab-ipadic-neologd-and-python3/
+
+        text = self._format_text(text)
+
+        mecab = MeCab.Tagger(
+            "-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd"
+        )
+
+        mecab.parse("")  # 文字列がGCされるのを防ぐ
+        node = mecab.parseToNode(text)
+        word_list = []
+        while node:
+            # 品詞を取得
+            pos = node.feature.split(",")[0]
+
+            if pos in ["名詞", "動詞", "形容詞", "副詞"]:
+                word = node.surface
+                word_list.append(word)
+            # elif pos in ["動詞", "形容詞"]:
+            # 次の単語に進める
+            node = node.next
+        ret = " ".join(word_list)
+        return ret
+
+    def _format_text(self, text):
+        """
+        MeCabに入れる前のツイートの整形方法例
+        """
+
+        text = re.sub(r"https?://[\w/:%#\$&\?\(\)~\.=\+\-…]+", "", text)
+        text = re.sub("RT", "", text)
+        text = re.sub("お気に入り", "", text)
+        text = re.sub("まとめ", "", text)
+        text = re.sub(r"[!-~]", "", text)  # 半角記号,数字,英字
+        text = re.sub(r"[︰-＠]", "", text)  # 全角記号
+        text = re.sub("\n", " ", text)  # 改行文字
+
+        return text
